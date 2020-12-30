f566a4c2647dc9aacffcec0d5036e86d61c2079e
Vadym Poberezhnyy <vpoberezhnyi@heliostech.fr>
2020-12-30 21:48:42 +0200

    parallax

--- src/components/landing-block/index.tsx
+++ src/components/landing-block/index.tsx
@@ -1,7 +1,5 @@
 import classNames from 'classnames';
 import React, { useCallback, useMemo } from 'react';
-import { ParallaxBanner } from 'react-scroll-parallax';
-import ScrollableAnchor from 'react-scrollable-anchor';
 import VisibilitySensor from 'react-visibility-sensor';
 
 import './index.scss';
@@ -12,9 +10,6 @@ interface LandingBlockProps {
     style?: React.CSSProperties;
     contentClassName?: string;
     contentStyle?: React.CSSProperties;
-    hashPath?: string;
-    parallaxImagePath?: string;
-    parallaxImageClassName?: string;
     onVisible?: (isVisible: boolean) => void;
 }
 
@@ -24,9 +19,6 @@ export const LandingBlock: React.FC<LandingBlockProps> = ({
     style,
     contentClassName,
     contentStyle,
-    hashPath,
-    parallaxImagePath,
-    parallaxImageClassName,
     onVisible,
 }) => {
     const handleChange = useCallback(
@@ -38,55 +30,23 @@ export const LandingBlock: React.FC<LandingBlockProps> = ({
         [onVisible]
     );
 
-    // top - hide
-    const content = useMemo(() => {
-        return (
-            <VisibilitySensor
-                onChange={handleChange}
-                partialVisibility={true}
-                offset={{ top: 500, bottom: 600 }}>
+    return (
+        <VisibilitySensor
+            onChange={handleChange}
+            partialVisibility={true}
+            offset={{ top: 500, bottom: 600 }}>
+            <div
+                className={classNames('landing-block', className)}
+                style={style}>
                 <div
-                    className={classNames('landing-block', className)}
-                    style={style}>
-                    <div
-                        className={classNames(
-                            'landing-block__content',
-                            contentClassName
-                        )}
-                        style={contentStyle}>
-                        {children}
-                    </div>
+                    className={classNames(
+                        'landing-block__content',
+                        contentClassName
+                    )}
+                    style={contentStyle}>
+                    {children}
                 </div>
-            </VisibilitySensor>
-        );
-    }, [handleChange, className, children, contentClassName]);
-
-    const scrollableContent = useMemo(() => {
-        return hashPath ? (
-            <ScrollableAnchor id={hashPath}>{content}</ScrollableAnchor>
-        ) : (
-            content
-        );
-    }, [content, hashPath]);
-
-    return parallaxImagePath ? (
-        <ParallaxBanner
-            style={{
-                height: 'auto',
-                minHeight: '100vh',
-            }}
-            layers={[
-                {
-                    image: parallaxImagePath,
-                    amount: 0.5,
-                    props: {
-                        className: parallaxImageClassName,
-                    },
-                },
-            ]}>
-            {scrollableContent}
-        </ParallaxBanner>
-    ) : (
-        scrollableContent
+            </div>
+        </VisibilitySensor>
     );
 };
