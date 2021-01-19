1dff36a1e4381de5ac481fe3c9ed65ca52a1dc6b
Vadym Poberezhnyy <vpoberezhnyi@heliostech.fr>
2021-01-19 16:10:21 +0200

    add widget frame

--- src/components/widget-frame/index.tsx
+++ src/components/widget-frame/index.tsx
@@ -10,11 +10,20 @@ interface Props {
     children: React.ReactNode;
 }
 
-export const WidgetFrame: React.FC<Props> = ({ header, className, children, style }) => {
+export const WidgetFrame: React.FC<Props> = ({
+    header,
+    className,
+    children,
+    style,
+}) => {
     return (
         <div className={classNames('widget-frame', className)} style={style}>
             <div className="widget-frame__title">
-                {typeof header === 'string' ? <div className="widget-frame__title-text">{header}</div> : header}
+                {typeof header === 'string' ? (
+                    <div className="widget-frame__title-text">{header}</div>
+                ) : (
+                    header
+                )}
             </div>
             <div className="widget-frame__children">{children}</div>
         </div>
